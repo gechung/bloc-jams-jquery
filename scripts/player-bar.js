c22bb4b0169519f7215ef08e8d452241009e3016
Grace <gechung@gmail.com>
2019-08-07 10:36:05 -0700

    Deployment

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -12,7 +12,7 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs [nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   setInterval( () => {
@@ -32,6 +32,15 @@
     if (currentSongIndex < 1) { return; }
 
     const previousSong = album.songs [previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
+  });
+
+  $('#time-control input').on('input', function (event) {
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+
+  $('#volume-control input').on('input',function (event) {
+    player.setVolume(event.target.value);
   });
 }
